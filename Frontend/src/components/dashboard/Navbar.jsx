d30dc445e9a70659f8343a40ed03d71c92ff970a
NikhilKumarSingh123456 <nikhil4dnpg@gmail.com>
2025-07-21 16:18:46 +0530

    update ignores files

--- Frontend/src/components/dashboard/Navbar.jsx
+++ Frontend/src/components/dashboard/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center text-white justify-between h-12 bg-teal-600 md:px-5">
         <p className='px-12'>Welcome {user.name} </p>
-        <button className="px-4 py-1 bg-teal-700 hover:bg-teal-800" onClick={logout}>Logout</button>
+        <button className="px-6 py-1 bg-teal-700 hover:bg-teal-800" onClick={logout}>Logout</button>
     </div>
   )
 }
